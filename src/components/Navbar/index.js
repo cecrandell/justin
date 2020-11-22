73fc9f5b212c59eae606a649f65bd7f9b0a35ac9
Caroline Crandell <carolineecrandell@gmail.com>
2020-11-22 02:01:01 -0800

    JSON added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,9 +5,9 @@ function Navbar(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="nav-item">A Collection of Justins</div>
-            <div className="nav-item">{props.message}</div>
             <div className="nav-item">Score: {props.score}</div>
             <div className="nav-item">Top Score: {props.topscore}</div>
+            <div className="nav-item">{props.message}</div>
         </nav>
     );
 
